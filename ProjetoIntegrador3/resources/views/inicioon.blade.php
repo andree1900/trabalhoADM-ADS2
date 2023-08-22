122c832afb348db06ebea7c20089219e397d5313
andree1900 <andreluisoliveira4@gmail.com>
2023-08-22 18:15:18 -0300

    Primeiro Commit desse teste.

---
+++ ProjetoIntegrador3/resources/views/inicioon.blade.php
@@ -0,0 +1,284 @@
+<div class=e155_161>
+  <div class=e155_162>
+    <div class=e155_168>
+      <div class=e161_298>
+        <div class=e155_169>
+          <div class=e230_2><span  class="e230_3">Favoritos</span></div>
+          <div class=e155_172><span  class="e155_173">Editar</span></div>
+        </div>
+      </div>
+      <div class=e155_174>
+        <div class=e155_175>
+          <div class=e465_1225>
+            <div class=e465_1226>
+              <div class=e465_1227>
+                <div class=e465_1228>
+                  <div class=e465_1229><span  class="e465_1230">Cadeira de Escritório</span></div>
+                  <div class=e465_1231><span  class="e465_1232">Modelo</span><span  class="e465_1233">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1234>
+                <div class="e465_1235"></div>
+              </div>
+              <div class=e465_1236>
+                <div class=e465_1237>
+                  <div class=e465_1238>
+                    <div class=e465_1239>
+                      <div class="e465_1240"></div>
+                    </div><span  class="e465_1241">Favoritos</span>
+                  </div>
+                  <div class=e465_1242>
+                    <div class=e465_1243>
+                      <div class="e465_1244"></div>
+                      <div class="e465_1245"></div>
+                    </div><span  class="e465_1246">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1247>
+            <div class=e465_1248>
+              <div class=e465_1249>
+                <div class=e465_1250>
+                  <div class=e465_1251><span  class="e465_1252">Cadeira de Escritório</span></div>
+                  <div class=e465_1253><span  class="e465_1254">Modelo</span><span  class="e465_1255">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1256>
+                <div class="e465_1257"></div>
+              </div>
+              <div class=e465_1258>
+                <div class=e465_1259>
+                  <div class=e465_1260>
+                    <div class=e465_1261>
+                      <div class="e465_1262"></div>
+                    </div><span  class="e465_1263">Favoritos</span>
+                  </div>
+                  <div class=e465_1264>
+                    <div class=e465_1265>
+                      <div class="e465_1266"></div>
+                      <div class="e465_1267"></div>
+                    </div><span  class="e465_1268">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1269>
+            <div class=e465_1270>
+              <div class=e465_1271>
+                <div class=e465_1272>
+                  <div class=e465_1273><span  class="e465_1274">Cadeira de Escritório</span></div>
+                  <div class=e465_1275><span  class="e465_1276">Modelo</span><span  class="e465_1277">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1278>
+                <div class="e465_1279"></div>
+              </div>
+              <div class=e465_1280>
+                <div class=e465_1281>
+                  <div class=e465_1282>
+                    <div class=e465_1283>
+                      <div class="e465_1284"></div>
+                    </div><span  class="e465_1285">Favoritos</span>
+                  </div>
+                  <div class=e465_1286>
+                    <div class=e465_1287>
+                      <div class="e465_1288"></div>
+                      <div class="e465_1289"></div>
+                    </div><span  class="e465_1290">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1291>
+            <div class=e465_1292>
+              <div class=e465_1293>
+                <div class=e465_1294>
+                  <div class=e465_1295><span  class="e465_1296">Cadeira de Escritório</span></div>
+                  <div class=e465_1297><span  class="e465_1298">Modelo</span><span  class="e465_1299">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1300>
+                <div class="e465_1301"></div>
+              </div>
+              <div class=e465_1302>
+                <div class=e465_1303>
+                  <div class=e465_1304>
+                    <div class=e465_1305>
+                      <div class="e465_1306"></div>
+                    </div><span  class="e465_1307">Favoritos</span>
+                  </div>
+                  <div class=e465_1308>
+                    <div class=e465_1309>
+                      <div class="e465_1310"></div>
+                      <div class="e465_1311"></div>
+                    </div><span  class="e465_1312">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1313>
+            <div class=e465_1314>
+              <div class=e465_1315>
+                <div class=e465_1316>
+                  <div class=e465_1317><span  class="e465_1318">Cadeira de Escritório</span></div>
+                  <div class=e465_1319><span  class="e465_1320">Modelo</span><span  class="e465_1321">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1322>
+                <div class="e465_1323"></div>
+              </div>
+              <div class=e465_1324>
+                <div class=e465_1325>
+                  <div class=e465_1326>
+                    <div class=e465_1327>
+                      <div class="e465_1328"></div>
+                    </div><span  class="e465_1329">Favoritos</span>
+                  </div>
+                  <div class=e465_1330>
+                    <div class=e465_1331>
+                      <div class="e465_1332"></div>
+                      <div class="e465_1333"></div>
+                    </div><span  class="e465_1334">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1335>
+            <div class=e465_1336>
+              <div class=e465_1337>
+                <div class=e465_1338>
+                  <div class=e465_1339><span  class="e465_1340">Cadeira de Escritório</span></div>
+                  <div class=e465_1341><span  class="e465_1342">Modelo</span><span  class="e465_1343">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1344>
+                <div class="e465_1345"></div>
+              </div>
+              <div class=e465_1346>
+                <div class=e465_1347>
+                  <div class=e465_1348>
+                    <div class=e465_1349>
+                      <div class="e465_1350"></div>
+                    </div><span  class="e465_1351">Favoritos</span>
+                  </div>
+                  <div class=e465_1352>
+                    <div class=e465_1353>
+                      <div class="e465_1354"></div>
+                      <div class="e465_1355"></div>
+                    </div><span  class="e465_1356">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1357>
+            <div class=e465_1358>
+              <div class=e465_1359>
+                <div class=e465_1360>
+                  <div class=e465_1361><span  class="e465_1362">Cadeira de Escritório</span></div>
+                  <div class=e465_1363><span  class="e465_1364">Modelo</span><span  class="e465_1365">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1366>
+                <div class="e465_1367"></div>
+              </div>
+              <div class=e465_1368>
+                <div class=e465_1369>
+                  <div class=e465_1370>
+                    <div class=e465_1371>
+                      <div class="e465_1372"></div>
+                    </div><span  class="e465_1373">Favoritos</span>
+                  </div>
+                  <div class=e465_1374>
+                    <div class=e465_1375>
+                      <div class="e465_1376"></div>
+                      <div class="e465_1377"></div>
+                    </div><span  class="e465_1378">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1379>
+            <div class=e465_1380>
+              <div class=e465_1381>
+                <div class=e465_1382>
+                  <div class=e465_1383><span  class="e465_1384">Cadeira de Escritório</span></div>
+                  <div class=e465_1385><span  class="e465_1386">Modelo</span><span  class="e465_1387">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1388>
+                <div class="e465_1389"></div>
+              </div>
+              <div class=e465_1390>
+                <div class=e465_1391>
+                  <div class=e465_1392>
+                    <div class=e465_1393>
+                      <div class="e465_1394"></div>
+                    </div><span  class="e465_1395">Favoritos</span>
+                  </div>
+                  <div class=e465_1396>
+                    <div class=e465_1397>
+                      <div class="e465_1398"></div>
+                      <div class="e465_1399"></div>
+                    </div><span  class="e465_1400">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class=e465_1401>
+            <div class=e465_1402>
+              <div class=e465_1403>
+                <div class=e465_1404>
+                  <div class=e465_1405><span  class="e465_1406">Cadeira de Escritório</span></div>
+                  <div class=e465_1407><span  class="e465_1408">Modelo</span><span  class="e465_1409">XMD-0092834.</span></div>
+                </div>
+              </div>
+              <div class=e465_1410>
+                <div class="e465_1411"></div>
+              </div>
+              <div class=e465_1412>
+                <div class=e465_1413>
+                  <div class=e465_1414>
+                    <div class=e465_1415>
+                      <div class="e465_1416"></div>
+                    </div><span  class="e465_1417">Favoritos</span>
+                  </div>
+                  <div class=e465_1418>
+                    <div class=e465_1419>
+                      <div class="e465_1420"></div>
+                      <div class="e465_1421"></div>
+                    </div><span  class="e465_1422">Reportar</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=e470_357>
+      <div class=e470_364>
+        <div class=e470_351>
+          <div class=e470_353><span  class="e470_354">Problemas Reportados</span></div>
+          <div class=e470_355></div>
+        </div>
+        <div class=e470_366></div>
+      </div>
+      <div class=e470_378>
+        <div class=e470_379>
+          <div class=e470_381><span  class="e470_382">Reclamações</span></div>
+          <div class=e470_383></div>
+        </div>
+        <div class=e470_385></div>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ ProjetoIntegrador3/resources/views/navon.blade.php
@@ -0,0 +1,21 @@
+<div class=e37_59>
+  <div class=e37_92>
+    <div class=e37_93>
+      <div class=e140_12><span  class="e37_94">Inicio</span></div>
+      <div class=e140_11><span  class="e37_95">Pesquisar</span></div>
+      <div class=e332_6><span  class="e332_7">Cadastros</span></div>
+      <div class=e465_1223><span  class="e465_1224">Caixa de Entrada</span></div>
+    </div>
+    <div class=e140_2></div>
+    <div class=e37_97>
+      <div class=e140_9><span  class="e140_10">Ajuda</span></div>
+      <div class=e140_6>
+        <div class="e32_205"></div>
+      </div>
+      <div class=e140_5>
+        <div class="e32_195"></div>
+        <div class="e32_192"></div>
+      </div>
+    </div>
+  </div>
+</div>
